c088690f506ccc700880768ddf3a1fbfe844ebf1
Giuseppe <cresctelli@gmail.com>
2025-11-22 06:58:15 +0100

    Built work page design

--- app/works/[slug]/page.tsx
+++ app/works/[slug]/page.tsx
@@ -30,6 +30,75 @@ export default async function page({ params }: Props) {
           ))}
         </div>
       </Section>
+      <Section className="grid grid-cols-[2fr_1fr] gap-8">
+        <div className="flex flex-col items-start">
+          <h3 className="text-xl font-medium ml-3 text-white">
+            Project Overview
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+          <h3 className="text-xl font-medium ml-3 mt-8 text-white">
+            Dettagli tecnici
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+
+          <h3 className="text-xl font-medium ml-3 mt-8 text-white">
+            Challenges
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+
+          <h3 className="text-xl font-medium ml-3 mt-8 text-white">
+            Key Features
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+
+          <h3 className="text-xl font-medium ml-3 mt-8 text-white">
+            Future Improvements
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+
+          <h3 className="text-xl font-medium ml-3 mt-8 text-white">
+            Lessons Learned
+          </h3>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            veritatis corrupti sit iure nihil molestias pariatur odit aliquid
+            nam a ullam, tenetur, illum explicabo ducimus illo velit alias et
+            dolorem. Repellendus reprehenderit in sed autem vero, accusantium
+            excepturi. Alias, iste.
+          </p>
+        </div>
+        <div className="bg-neutral-950 border border-neutral-800 rounded-md p-4"></div>
+      </Section>
     </main>
   );
 }
