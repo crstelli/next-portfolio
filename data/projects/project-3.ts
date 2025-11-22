7836672b26a805b080c1f5ef2b774b70708c1728
Giuseppe <cresctelli@gmail.com>
2025-11-22 13:27:53 +0100

    Implemented full MDX Documentation for third project, lumina tech

--- data/projects/project-3.ts
+++ data/projects/project-3.ts
@@ -3,24 +3,54 @@ import type { Project } from "./type";
 export const project3: Project = {
   slug: "lumina-tech",
   stack: "Frontend",
-  date: { year: 2025, month: "October" },
-  worked_for: "4 days",
+  date: { year: 2025, month: "November" },
+  worked_for: "3 days",
   hosted_on: "Vercel",
-  name: "Shoppy Dashboard",
+  name: "Lumina Tech",
   description:
-    "Shoppy is a modern e-commerce platform where users can order a variety of digital and physical products, filter them by price and status, and track their orders.",
+    "LuminaTech is a fully static, design-focused website built with Next.js and TailwindCSS, showcasing a fictional tech company through clean layouts, modern UI, and smooth responsive components.",
   technologies: [
-    "Next.js",
-    "React",
-    "TailwindCSS",
-    "Typescript",
-    "Supabase",
-    "Vercel",
-    "Auth.js",
-    "date-fns",
-    "lucide-react",
-    "react-hot-toast",
-    "react-range",
+    {
+      name: "Next.js",
+      icon: "/techstack-logos/nextjs.svg",
+      description: "desc",
+    },
+
+    {
+      name: "React",
+      icon: "/techstack-logos/react.svg",
+      description: "desc",
+    },
+
+    {
+      name: "TailwindCSS",
+      icon: "/techstack-logos/tailwindcss.svg",
+      description: "desc",
+    },
+
+    {
+      name: "Typescript",
+      icon: "/techstack-logos/typescript.svg",
+      description: "desc",
+    },
+
+    {
+      name: "Vercel",
+      icon: "/techstack-logos/vercel.svg",
+      description: "desc",
+    },
+
+    {
+      name: "Embla Carousel",
+      icon: "",
+      description: "desc",
+    },
+
+    {
+      name: "lucide-react",
+      icon: "",
+      description: "desc",
+    },
   ],
   features: ["Responsive Design", "Login with Google", "Order Tracking"],
   github: "https://github.com/crstelli/lumina-tech",
