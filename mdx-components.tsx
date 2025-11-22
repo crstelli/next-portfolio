031d0b67491a9b9146f40cdfb66a51dc1a0ff1dd
Giuseppe <cresctelli@gmail.com>
2025-11-22 11:23:57 +0100

    Built MDX for first project page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,15 @@
 import type { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
-  h1: ({ children }) => <h1 className="text-xl font-bold ml-4">{children}</h1>,
+  h1: ({ children }) => <h1 className="text-2xl font-bold ml-4">{children}</h1>,
   h2: ({ children }) => (
-    <h2 className="text-xl font-bold ml-4 mt-8">{children}</h2>
+    <h2 className="text-xl font-bold ml-4 mt-12 mb-2">{children}</h2>
   ),
-  p: ({ children }) => <p className="text-neutral-400">{children}</p>,
+  p: ({ children }) => <p className="text-neutral-400 my-3">{children}</p>,
+  strong: ({ children }) => <strong className="text-white">{children}</strong>,
+  em: ({ children }) => <em className="text-white">{children}</em>,
+  ul: ({ children }) => <ul className="text-primary list-disc">{children}</ul>,
+  ol: ({ children }) => <ol className="list-decimal">{children}</ol>,
 };
 
 export function useMDXComponents(): MDXComponents {
