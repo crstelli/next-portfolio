ae0c468b2dd97ae9fd5d6065f07a1389e0a9ceb1
Giuseppe <cresctelli@gmail.com>
2025-11-23 12:27:00 +0100

    Fixed navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import { Item } from "./Item";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="flex sticky top-0 border border-neutral-900 rounded-b-md backdrop-blur-sm px-6 z-1 self-center py-5 items-center text-lg justify-between w-full max-w-[1300px]">
-      <div className="relative aspect-10/1 w-50">
+      <Link href="/" className="relative aspect-10/1 w-50">
         <Image
           src="/logo.png"
           alt="Logo"
           className="object-cover object-center"
           fill
         />
-      </div>
+      </Link>
       <ul className="flex items-center gap-4">
         <Item customRef="/">Home</Item>
         <Item>Works</Item>
