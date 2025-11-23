b7ff2486fd9bd31a515d84da02d738b13e391c7c
Giuseppe <cresctelli@gmail.com>
2025-11-23 12:26:22 +0100

    Added logo

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import { Item } from "./Item";
 
 function Navbar() {
   return (
-    <nav className="flex sticky top-0 border border-neutral-900 rounded-b-md backdrop-blur-sm px-4 z-1 self-center py-5 items-center text-lg justify-between w-full max-w-[1300px]">
-      <span>Logo</span>
+    <nav className="flex sticky top-0 border border-neutral-900 rounded-b-md backdrop-blur-sm px-6 z-1 self-center py-5 items-center text-lg justify-between w-full max-w-[1300px]">
+      <div className="relative aspect-10/1 w-50">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          className="object-cover object-center"
+          fill
+        />
+      </div>
       <ul className="flex items-center gap-4">
         <Item customRef="/">Home</Item>
         <Item>Works</Item>
