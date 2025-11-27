5be9c8a9192fda02595ca632dafa409b4b028d0d
Giuseppe <cresctelli@gmail.com>
2025-11-27 07:11:44 +0100

    Implemented animation to title and subtitles

--- components/section/Subtitle.tsx
+++ components/section/Subtitle.tsx
@@ -1,9 +1,22 @@
+"use client";
+
+import { motion } from "motion/react";
+
 interface Props {
   children: string;
 }
 
 function Subtitle({ children }: Props) {
-  return <p className="text-center max-sm:text-sm mt-6 italic max-w-[800px] text-neutral-500 mx-auto">{children}</p>;
+  return (
+    <motion.p
+      initial={{ y: 20, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ margin: "-200px", once: true }}
+      className="text-center max-sm:text-sm mt-6 italic max-w-[800px] text-neutral-500 mx-auto"
+    >
+      {children}
+    </motion.p>
+  );
 }
 
 export { Subtitle };
--- components/section/Title.tsx
+++ components/section/Title.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { motion } from "motion/react";
+
 interface Props {
   children: string;
 }
 
 function Title({ children }: Props) {
   return (
-    <h2 className="text-center text-xl sm:text-2xl md:text-3xl uppercase">
+    <motion.h2
+      initial={{ y: 20, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ margin: "-200px", once: true }}
+      className="text-center text-xl sm:text-2xl md:text-3xl uppercase"
+    >
       <Separator />
       {children}
       <Separator />
-    </h2>
+    </motion.h2>
   );
 }
 
